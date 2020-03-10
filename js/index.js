5f4889739f235874b1628d64921f8b26dfa15b38
evan <aspaasevan@gmail.com>
2020-03-10 14:58:48 -0700

    keydown and dblclick event listeners added

--- js/index.js
+++ js/index.js
@@ -1,7 +1,11 @@
-// selector for heading logo
+/*
+ * mouseenter and mouseleave event listeners change text color
+ */
+
+ // selector for heading logo
 const logo = document.querySelector('.logo-heading');
 
-// mouseenter and mouseleave event listeners change color 
+// event listeners
 logo.addEventListener('mouseenter', event => {
   event.target.style.color = "yellow";
 });
@@ -9,13 +13,49 @@ logo.addEventListener('mouseleave', event => {
   event.target.style.color = "black";
 });
 
+
+/*
+ * mouseover event listener to each nav link
+ */
+
 // selector for nav links
 const link = document.querySelectorAll('.nav-link');
 
-// add mouseover event listener to each nav link
+// event listeners
 link.forEach(link => {
     link.addEventListener('mouseover', event => {
         event.target.style.color = "yellow";
     });
 });
 
+/*
+ * keydown event listener stores key pressed in an array
+ */
+
+// initalize a new array
+const keyArray = [];
+
+// event listener
+document.addEventListener('keydown', event => {
+  keyArray.push(event.code);
+  console.log(keyArray);
+});
+
+
+/*
+ * dblclick event listener on bus image makes it change src paths
+ */
+
+// selector for bus image
+const busImage = document.querySelector('.intro img');
+
+// event listener
+busImage.addEventListener('dblclick', event => {
+  event.target.src = "img/vw_bus.jpg";
+  event.target.setAttribute('alt', "vw bus");
+});
+
+// add drag/drop event listener
+// add load event listener
+// add focus event listener
+// add click event listener for button
